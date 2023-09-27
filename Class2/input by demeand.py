51a9ecb48106ce9c0200aae6802e9fb4a531295f
EranStudents <131552121+EranStudents@users.noreply.github.com>
2023-09-27 15:24:52 +0300

    Create input by demeand.py

---
+++ Class2/input by demeand.py
@@ -0,0 +1,10 @@
+# הפעולה input בברירת המחדל שלה היא משתנה מסוג מחרוזת
+
+name = input("Enter your name :")
+print(type(name))  # str
+
+age = int(input("Enter your age :"))
+print(type(age))  # int
+
+avg = float(input("Enter your avg in English"))
+print(type(avg))  # float
