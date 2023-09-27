28fdc81ca7364f5be7f59681c40a5db37ac6f1d8
EranStudents <131552121+EranStudents@users.noreply.github.com>
2023-09-27 15:24:23 +0300

    Create Class_2.py

---
+++ Class2/Class_2.py
@@ -0,0 +1,27 @@
+'''
+
+string - str - מחרוזת - טקסט כלשהו
+integer - int - מספר שלם - 68,23,-96
+float - מספר עשרוני - 64.23
+
+name = "Eran"
+type - סוג
+הפעולה type מאפשרת לנו לדעת מה סוג המשתנה
+
+איך אני מדפיס טקסט ביחד עם משתנים ?
+
+print(f'My name {var} ... ')
+'''
+
+name = "Eran"
+print(name)
+print(type(name))
+num = 85
+num2 = 47.5
+
+print(f'Hello my name is {name} and my age is {num}')
+
+print(type(num))
+print(type(num2))
+
+
